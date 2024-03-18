af86cfcf7dda4f34bd4b57cedb403a12c6e78258
Winícius Oliveira De Andrade <winiciusa@ciandt.com>
2024-03-18 09:45:20 -0300

    test

--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -1,5 +1,5 @@
 
-it.only('testa a pagina da política de privacidade de forma independente', () => {
+it('testa a pagina da política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html')
     cy.contains('Talking About Testing').should('be.visible')
 });
